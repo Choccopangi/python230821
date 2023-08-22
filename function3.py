52df3953d634226169a7a1b3336fb19c6ac486f4
Choccopangi <thdus7580@gmail.com>
2023-08-22 09:45:46 +0900

    함수 연습 완료

---
+++ function3.py
@@ -0,0 +1,40 @@
+# function3.py
+
+# 가변 인자 처리 (*는 tuple, **은 dict를 의미)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+# 함수를 호출
+print(union("HAM", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
+
+# 람다함수(이름이 없는 간단한 함수 정의)
+g = lambda x,y : x*y
+print(g(3,4), g(5,6))
+print((lambda x : x*x)(3))
+print(globals())
+
+print("---필터함수---")
+lst = [10,25,30]
+iterL = filter(None, lst)
+for item in iterL:
+    print(item)
+
+# 필터링하는 함수 정의
+def getBiggerThan20(i):
+    return i>20
+
+print("---일반함수---")
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+print("---람다함수---")
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print(item)
